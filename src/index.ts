3350fd9219409243cfb9cb53d14f3d104781694b
Roman <email.rom.melnyk@gmail.com>
2018-05-12 17:48:25 +0200

    Observable: from string + from fetch()

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import { Observable } from 'rxjs';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
 
 
 function runDemo() {
   console.info('It works!');
 
-  const stream1 = Observable.of('https://api.github.com/users');
+  const stream1 = Observable
+    .of('https://api.github.com/users')
+    .mergeMap((val: string) => Observable.fromPromise(
+      fetch(val)
+        .then((res: Response) => res.json())
+    ));
+
   stream1.subscribe((val) => {
     console.log(val);
   })
+}
+
+
+if (window) {
+  Object.assign(window, { runDemo });
 }
