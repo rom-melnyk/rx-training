fd62ebc3fc021873474b9a411c6e29039a9fc0c5
Roman <email.rom.melnyk@gmail.com>
2018-05-21 20:12:24 +0200

    Delete button action (finished)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { Observable } from 'rxjs';
 import 'rxjs/add/observable/of';
-import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/map';
@@ -22,21 +21,21 @@ function runDemo() {
   const refreshButton: HTMLElement = document.querySelector('.refresh') as HTMLElement;
   const rowsContainer: HTMLElement = document.querySelector('.rows') as HTMLElement;
 
-  // render initial (empty) list to receive button HTML elements
+  // render initial (empty) list to create button HTML elements
   const initialUserData = Array(USER_COUNT).fill({ is_hidden: true });
   render(allRowsTemplate(initialUserData as GitUser[]), rowsContainer);
   const deleteButtons: HTMLElement[] = Array.from(
     document.querySelectorAll('a.delete')
   );
 
-  // create a combined stream from all the delete button clicks (<index-in-del-button>)
-  let deleteButtonsStream = Observable.from(
-    Array(USER_COUNT).fill(0).map((val, i) => i)
-  );
+  let deleteButtonsStream = Observable.of({ timestamp: Date.now(), index: 0 }); // initial value
   deleteButtons.forEach((button: HTMLElement, i: number) => {
+    // TODO multiple clicks emitted
     const buttonStream = Observable.fromEvent(button, 'click')
       .do(() => { console.log(`Delete button #${i} clicked`); })
-      .mapTo(i);
+      .map(() => {
+        return { timestamp: Date.now(), index: i };
+      });
     deleteButtonsStream = deleteButtonsStream.merge(buttonStream);
   }, );
 
@@ -59,13 +58,21 @@ function runDemo() {
     .mergeMap((url: string) => Observable.fromPromise(
       fetch(url)
         .then((res: Response) => res.json())
-        .then((values: object[]) => values.slice(0, USER_COUNT))
+        .then((values: object[]) => {
+          return { timestamp: Date.now(), users: values.slice(0, USER_COUNT) };
+        })
     ))
-    // .mergeMap((users: object[]) => deleteButtonsStream.map((i: number) => {
-    //     Object.assign(users[i], { is_hidden: true });
-    //     return users;
-    //   })
-    // );
+    .combineLatest(deleteButtonsStream)
+    .map((data: [{ timestamp: number, users: object[] }, { timestamp: number, index: number }]) => {
+      const [
+        { users, timestamp: fetchTimestamp },
+        { index, timestamp: delTimestamp }
+      ] = data;
+      if (delTimestamp > fetchTimestamp) {
+        Object.assign(users[index], { is_hidden: true });
+      }
+      return users;
+    });
 
   fetchUsersStream.subscribe((val) => {
     // console.log(val);
